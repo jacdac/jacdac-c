6640a063d13f72e4c72338f8bbe0613a1490b239
Thomas Ball <thomas.j.ball@outlook.com>
2025-05-23 03:39:16 -0700

    new service start!

--- services/bitmap.c
+++ services/bitmap.c
@@ -1,1 +1,30 @@
-// TODO: code me up!
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+#include "jd_services.h"
+#include "jacdac/dist/c/bitmap.h"
+
+struct srv_state {
+    SRV_COMMON;
+};
+
+
+void bitmap_process(srv_t *state) {
+    // likely a NOP
+}
+
+void bitmap_handle_packet(srv_t *state, jd_packet_t *pkt) {
+    switch (pkt->service_command) {
+    case JD_BITMAP_CMD_FILL:
+        // TBD
+        break;
+    default:
+        // TBD service_handle_register_final(state, pkt, buzzer_regs);
+        break;
+    }
+}
+
+// SRV_DEF(buzzer, JD_SERVICE_CLASS_BUZZER);
+void bitmap_init(uint16_t width, uint16_t height) {
+    // TBD
+}
